18757f380dac1f129e4ddfc93d3e295c4cfd86cf
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-11-26 09:24:25 -0800

    Rendered  the messages on the web page

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template, request
 # request is imported to read data sent from the browser
 app = Flask(__name__) #App Instance
 
+messages = []
+
 #Route Decorator
 @app.route("/", methods = ["GET", "POST"])
 def home():
     if request.method ==  "POST":
         # Get the message from the form input
-        message = request.form.get("message")
-        print("New message from form:", message)
+        message = request.form.get("message", "").strip()
 
-    return render_template("index.html")
+        if message: 
+            messages.append(message)
+            print("New message from form:", message)
+
+    # Pass messages to the template
+    return render_template("index.html", messages = messages)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True, port = 5050)
