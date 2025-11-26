ef0ab2e3eb4c37b043a1e71f72c84980b6929685
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-11-25 16:52:43 -0800

    Implemented join and leave chatroom methods

--- chatroom.py
+++ chatroom.py
@@ -13,10 +13,30 @@ class Chatroom:
 class User:
     def __init__(self, user_name):
         self.user_name = user_name
-    
+        self.chatroom = None
+
+    def join_chatroom(self, chatroom):
+        if(self.chatroom):
+            print(f"{self.user_name} is already in Chatroom.")
+        else:
+            chatroom.add_user(self)
+            self.chatroom = chatroom
+            print(f"{self.user_name} has joined {chatroom.room_name}")
+
+    def leave_chatroom(self, chatroom):
+        if not self.chatroom:
+            print(f"{self.user_name} is not in any Chatroom")
+        
+        
+
 
 room = Chatroom("Jujutsu High Group Chat")
+
 user1 = User("Satoru Gojo")
+user2 = User("Ryomen Sukuna")
+
+user1.join_chatroom(room)
+user2.join_chatroom(room)
 
 
 
