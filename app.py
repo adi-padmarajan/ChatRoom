0d88e83a9b6317753172c7995074ebd2a8c23d17
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-11-26 08:06:42 -0800

    Started index.html and used render_template to render it on the web page from app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) #App Instance
 
 #Route Decorator
 @app.route("/")
 def home():
-    return "Chatroom is running"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
