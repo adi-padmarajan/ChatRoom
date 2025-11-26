0df2b04560f1fe11b4e5027715c8c4e3bad6024a
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-11-26 09:09:40 -0800

    Updated app.py to get messages using methods "GET" and "POST"

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+# request is imported to read data sent from the browser
 app = Flask(__name__) #App Instance
 
 #Route Decorator
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def home():
+    if request.method ==  "POST":
+        # Get the message from the form input
+        message = request.form.get("message")
+        print("New message from form:", message)
+
     return render_template("index.html")
 
 if __name__ == "__main__":
