5506f48eb1bd2af9f02a99a315b49e628ead179e
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-11-26 15:57:35 -0800

    routing from welcome to the chat is working

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
 from flask import Flask, render_template, request, redirect
-# request is imported to read data sent from the browser
+from chatroom import Chatroom, User  # import from chatroom.py
 
-from chatroom import Chatroom, User #import from chatroom.py
-app = Flask(__name__) #App Instance
+app = Flask(__name__)
 
-#Create Chatroom
-chatroom = Chatroom("Jujutsu High School")
+# ---------- Create Chatroom and Users ----------
+
+chatroom = Chatroom("HCI Research Project")
 
 current_user = User("You")
-gojo = User("Satoru Gojo")
-sukuna = User("Ryomen Sukuna")
+jasmine = User("Jasmine")
+brian = User("Brian")
+madi = User("Madison")
+fintan = User("Fintan")
 
 chatroom.add_user(current_user)
-chatroom.add_user(gojo)
-chatroom.add_user(sukuna)
+chatroom.add_user(jasmine)
+chatroom.add_user(brian)
+chatroom.add_user(madi)
+chatroom.add_user(fintan)
 
-chatroom.broadcast_message(sukuna, "I feel bad taking on a handicap.")
-chatroom.broadcast_message(gojo, "I am still standing")
+chatroom.broadcast_message(jasmine, "Are we all down to meet tomorrow for our presentation prep.")
+chatroom.broadcast_message(madi, "Yes! I will have my script ready.")
+chatroom.broadcast_message(brian, "What is our presentation time limit? ")
+chatroom.broadcast_message(fintan, "No more than 7 minutes, so probably around 1.5 minutes of speaking time for each.")
 
 
-#Root -> send people to the landing page
+# ---------- Routes ----------
+
+# Root -> send people to the landing page
 @app.route("/")
 def root():
     return redirect("/welcome")
@@ -31,26 +39,26 @@ def welcome():
     return render_template("welcome.html")
 
 
-#Route Decorator
-@app.route("/", methods = ["GET", "POST"])
-def home():
-    if request.method ==  "POST":
+# Main chat page
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+    if request.method == "POST":
         # Get the message from the form input
         message_text = request.form.get("message", "").strip()
 
-        if message_text: 
-            # Use existing OOP
+        if message_text:
             chatroom.broadcast_message(current_user, message_text)
             print("New message from form:", message_text)
 
     # Pass messages to the template
     return render_template(
-        "index.html", 
-        messages = chatroom.messages,
-        room_name = chatroom.room_name,
+        "index.html",
+        messages=chatroom.messages,
+        room_name=chatroom.room_name,
         online_count=len(chatroom.users),
-        current_user_name = current_user.user_name
+        current_user_name=current_user.user_name,
     )
+
 
 @app.route("/create")
 def create():
@@ -59,7 +67,9 @@ def create():
 
 @app.route("/join")
 def join():
-    return "Join chatroom page coming soon"
+    return redirect("/chat")
+
+
 
 if __name__ == "__main__":
-    app.run(debug = True, port = 5050)
+    app.run(debug=True, port=5050)
